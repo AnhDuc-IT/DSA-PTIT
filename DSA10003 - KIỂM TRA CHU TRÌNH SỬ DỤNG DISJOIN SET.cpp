882d48b812ef5cff97dfb65aff1a88be88047182
AnhDuc-IT <duckuro.ptit@gmail.com>
2025-05-14 19:27:55 +0700

    Update DSA10003 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cpp

--- DSA10003 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cpp
+++ DSA10003 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cpp
@@ -34,8 +34,9 @@ bool Union(int u, int v){
     return true;
 }
 int main(){
-    ios :: sync_with_stdio(false);
-    cin.tie(0);
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int t; cin >> t;
     while(t--){
         cin >> n >> m;
